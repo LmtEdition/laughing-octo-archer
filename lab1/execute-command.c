bda0e6e263cd2f6ca657905714c4c5ae892ab1b3
Anthony Su <antchovee@gmail.com>
2013-01-29 20:58:06 -0800

    1. updated execute_command return a boolean type 2. implementation of all command excluding pipe

--- lab1/execute-command.c
+++ lab1/execute-command.c
@@ -6,12 +6,15 @@
 #include <error.h>
 #include <stdbool.h> //bool
 
+#include <fcntl.h>
+#include <unistd.h>
+
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
 // 1 indicates command failed
 // 0 indicates command success
-
+/*
 bool exec_andor(command_t c) {
    
     //When parallezing, WAIT FOR EXIT VALUES
@@ -21,7 +24,7 @@ bool exec_andor(command_t c) {
         //If the left side evaluates to true we must also evaluate the left side
         //Otherwise return false indicating that command failed
 
-        return execute_command(c->command[0]) &&  execute_command(c->command[1]);   
+        return execute_command(c->u.command[0]) &&  execute_command(c->u.command[1]);   
   
 
     } else {
@@ -29,26 +32,26 @@ bool exec_andor(command_t c) {
         // If the left side is true return 0 (success immediately)
         // Otherwise also evaluate right side
 
-        return execute_command(c->command[0]) || execute_command(c->command[1]);
+        return execute_command(c->u.command[0]) || execute_command(c->u.command[1]);
 
     }
 }
-
+*/
 /**
  *
  *  Execution for sequence command ;
  *  executes left and right command and returns 0 indicating that sequence was successfully executed
  *
  */
-
-bool exec_sequence(command_t) {
+/*
+bool exec_sequence(command_t c) {
 
 
     //execute left
-    execute_command(c->command[0]);
+    execute_command(c->u.command[0]);
 
     //execute right
-    execute_command(c->command[1]);
+    execute_command(c->u.command[1]);
 
     //essentially a place holder because postorder traversal executes left the right
     return true;
@@ -59,18 +62,51 @@ bool exec_pipe(command_t c){
 }
 
 bool exec_simple(command_t c) {
-    Includes redirect handling 
     //file io
     //if input redirection <
-    if(c->input)
-        int fd = open(pathname,...);
-        read(fd,...);
-        close(fd);
+    int fd_i , fd_o;
+
+    if(c->input) {
+
+        if((fd_i = open(input,O_RDONLY)) == -1)) {
+
+           error(1, 0, "Error opening input file: %s\n", c->input);
+
+        } else {
+            //closes fd and and performs redirection to specified file descriptor 
+            //such that input is read from file
+            if(dup2(fd_i,0) < 0) {
+                error(1, 0, "Error using dup2 for input redirection");
+            }
+        }
+    }
+
     //if output redirection
-    if(c->output)
-        int fd = open(pathname,....);
-        write(fd,...);
-        close(fd);
+    if(c->output) {
+        if((fd_o = open(output,O_WRONLY)) == -1) {
+            error(1, 0, "Error opening output file: %s\n", c->output);
+        } else {
+             //duplicates output fd into stdout file descriptor such that output redirects into file  
+             if(dup2(fd_o,1) < 0) {
+                   error(1, 0, "Error using dup2 for output redirection");
+             }
+        }
+    }
+
+    //finally execute function
+    if(execvp(c->u.word[0],c->u.word) == -1) {
+        return false;
+    }
+
+    if(fd_i != -1) {
+        close(fd_i);
+    }
+
+    if(fd_o != -1) {
+        close(fd_o);
+    }
+
+    return true;
 
 }
 
@@ -79,7 +115,7 @@ bool exec_subshell(command_t c) {
     //return the value of the single child ; essentially a place holder
     return exec_command(c->subshell_command);
  }
-
+*/
 
 int command_status (command_t c)
 {
@@ -101,30 +137,93 @@ execute_command (command_t c, bool time_travel)
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  
   error (1, 0, "command execution not yet implemented");*/
-        
-    //PSEUDOCODE    
-    if(command_t == NULL)
+
+    if(c == NULL)
         return true;
 
     switch(c->type) {
 
-    //binary commands
-    case AND_COMMAND:
-    case OR_COMMAND:
-        return execute_andor(c);
-    case SEQUENCE_COMMAND:
-        return exec_sequence(c);
-  
-    case PIPE_COMMAND:
-        return exec_pipe(c);
+        //binary commands
+        case AND_COMMAND:
 
-    //unary commands
-    case SIMPLE_COMMAND:
-        return exec_simple(c);
-    case SUBSHELL_COMMAND:
-        return exec_subshell(c);
-    default:
-          abort ();
+            //If the left side evaluates to true we must also evaluate the left side
+            //Otherwise return false indicating that command failed
+
+           return execute_command(c->u.command[0],time_travel) &&  execute_command(c->u.command[1],time_travel);  
+
+        case OR_COMMAND:
+            // If the left side is true return 0 (success immediately)
+            // Otherwise also evaluate right side
+            return execute_command(c->u.command[0],time_travel) || execute_command(c->u.command[1],time_travel);
+
+        case SEQUENCE_COMMAND:
+        {   //execute left
+            execute_command(c->u.command[0],time_travel);
+
+            //execute right
+            execute_command(c->u.command[1],time_travel);
+
+            //essentially a place holder because postorder traversal executes left the right
+            return true;
+        }
+        case PIPE_COMMAND:
+            
+            //return exec_pipe(c);
+            //TODO
+            return true;
+
+        //unary commands
+        case SIMPLE_COMMAND:
+        {    //file io
+            //if input redirection <
+            int fd_i , fd_o;
+
+            if(c->input) {
+
+                if((fd_i = open(c->input,O_RDONLY)) == -1) {
+
+                   error(1, 0, "Error opening input file: %s\n", c->input);
+
+                } else {
+                    //closes fd and and performs redirection to specified file descriptor 
+                    //such that input is read from file
+                    if(dup2(fd_i,0) < 0) {
+                        error(1, 0, "Error using dup2 for input redirection");
+                    }
+                }
+            }
+
+            //if output redirection
+            if(c->output) {
+                if((fd_o = open(c->output,O_WRONLY)) == -1) {
+                    error(1, 0, "Error opening output file: %s\n", c->output);
+                } else {
+                     //duplicates output fd into stdout file descriptor such that output redirects into file  
+                     if(dup2(fd_o,1) < 0) {
+                           error(1, 0, "Error using dup2 for output redirection");
+                     }
+                }
+            }
+
+            //finally execute function
+            if(execvp(c->u.word[0],c->u.word) == -1) {
+                return false;
+            }
+
+            if(fd_i != -1) {
+                close(fd_i);
+            }
+            
+            if(fd_o != -1) {
+                close(fd_o);
+            }
+
+            return true;
+        }
+        case SUBSHELL_COMMAND:
+            return execute_command(c->u.subshell_command,time_travel);
+        default:
+              abort ();
     }
 
 }
