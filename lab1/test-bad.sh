db555ab344a25f527c8a74aa139b3f69787d8fa7
Stanley Xu <stanley.wux@gmail.com>
2013-02-03 23:37:05 -0800

    1. added test-bad.sh - tests 1b bad commands

---
+++ lab1/test-bad.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+# UCLA CS 111 Lab 1b - Test that execution works properly. 
+
+tmp=$0-$$.tmp
+mkdir "$tmp" || exit
+
+(
+cd "$tmp" || exit
+
+cat >test.sh <<'EOF'
+
+ahegiealg
+mv hageilha hagiehlag
+ls -agehialg
+cat < eagaegag
+sort ahegiagehl
+EOF
+
+cat >test.exp <<'EOF'
+blahNo such file or directory
+mv: cannot stat `hageilha': No such file or directory
+ls: invalid option -- 'e'
+Try `ls --help' for more information.
+No such file or directory
+sort: open failed: ahegiagehl: No such file or directory
+EOF
+
+../timetrash test.sh 2>test.out || exit
+
+diff -u test.exp test.out || exit
+test ! -s test.err || {
+  cat test.err
+  exit 1
+}
+
+) || exit
+
+rm -fr "$tmp"
